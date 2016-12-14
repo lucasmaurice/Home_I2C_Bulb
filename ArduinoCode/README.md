5b6ee16df6304e6480a09a77443cd5a4cc275630
Lucas Maurice <lucas.maurice@outlook.com>
2016-12-14 17:26:29 -0500

    Add semi complete Readme for arduino code

---
+++ ArduinoCode/README.md
@@ -0,0 +1,33 @@
+# Arduino Code for Home-Fake-RGB
+In order to use this module you'll need an arduino with a specific program burned in.
+
+# Without RF Module
+If you do not need to use the RF module, you just need the server code
+Remove all the lines between :
+
+```cpp
+//YOU NEED TO OVERWRITE THE FOLLOWING LINES:
+```
+and the next 
+```cpp 
+//STOP
+```
+
+Set the macro definition USERF to false :
+```cpp
+#define USERF false
+```
+
+# How to use received data:
+All data received where stored in an array of bytes.
+Some keys where already defined to simplify the access of good lines in the array :
+* **KEYRED** : Read the __RED__ intensity
+* **KEYGREEN** : Read the __GREEN__ intensity
+* **KEYBLUE** : Read the __BLUE__ intensity
+* **KEYBULB** : Read the bulb number
+* **KEYRADIO** : Read the radio adress (only if you use radio modules)
+
+In order to access the data just use the following command :
+```cpp
+byte bValue = bMessage[THEKEY];
+```
--- ArduinoCode/server/server.ino
+++ ArduinoCode/server/server.ino
@@ -11,8 +11,12 @@
 #define SIZE_MSG 5
 
 //Definition des constante RF
+//SET USERF TO FALSE IF YOU DONT NEED THAT
 #define ADDR_RECEPT 01
+#define USERF true
 uint8_t addr_recep[5] = {'n','r','f','0'+(ADDR_RECEPT/10)%10,'0'+ADDR_RECEPT%10};
+
+//KEY FOR USE I2C DATA
 #define KEYRED 0
 #define KEYGREEN 1
 #define KEYBLUE 2
@@ -23,21 +27,19 @@ void setup() {
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
 
+  if(USERF){
+    Mirf.cePin = 7; // Broche CE sur D9
+    Mirf.csnPin = 8; // Broche CSN sur D10
+    Mirf.spi = &MirfHardwareSpi; // On veut utiliser le port SPI hardware
+    Mirf.init(); // Initialise la bibliothèque
 
-  Mirf.cePin = 7; // Broche CE sur D9
-  Mirf.csnPin = 8; // Broche CSN sur D10
-  Mirf.spi = &MirfHardwareSpi; // On veut utiliser le port SPI hardware
-  Mirf.init(); // Initialise la bibliothèque
+    Mirf.channel = 1; // Choix du canal de communication (128 canaux disponibles, de 0 à 127)
+    Mirf.payload = 16; // Taille d'un message (maximum 32 octets)
+    Mirf.config(); // Sauvegarde la configuration dans le module radio
 
-  Mirf.channel = 1; // Choix du canal de communication (128 canaux disponibles, de 0 à 127)
-  Mirf.payload = 16; // Taille d'un message (maximum 32 octets)
-  Mirf.config(); // Sauvegarde la configuration dans le module radio
-
-
-
-  //Mirf.setTADDR(addr); // Adresse de transmission
-  Mirf.setRADDR(addr_recep); // Adresse de réception
-
+    //Mirf.setTADDR(addr); // Adresse de transmission
+    Mirf.setRADDR(addr_recep); // Adresse de réception
+  }
 
   Serial.begin(9600);           // start serial for output
 }
@@ -56,12 +58,20 @@ void receiveEvent(int howMany) {
     for(int iLoop=0; iLoop<SIZE_MSG; iLoop++){
        bMessage[iLoop]=Wire.read();
     }
+    //ENTER HERE WHAT TO DO ON I2C DATA RECEPTION:
+    //USE THE FOLLOWING KEY TO USE DATA:
+    //bMessage[KEY]
+    //KEYBULB : ID of the bulb
+    //KEYRED - KEYGREEN - KEYBLUE : intensity of the different colors
+    //KEYRADIO : used for RF module only, Adress of the receiving rf module
 
+    //YOU NEED TO OVERWRITE THE FOLLOWING LINES:
     addr_emit[3]='0'+(bMessage[KEYRADIO]/10)%10;
     addr_emit[4]='0'+ bMessage[KEYRADIO]%10;
     while(Mirf.isSending());
     Mirf.setTADDR(addr_emit); // Adresse de transmission
     Mirf.send(bMessage);
+    //STOP
   }
   delay(100);
 }
